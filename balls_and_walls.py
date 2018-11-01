2929665ea39af94bd4e130dd6187fc8a9955b271
StGalya <stupnikovagalunya@yandex.ru>
2018-11-01 19:34:29 +0300

    исправила текст проги

--- balls_and_walls.py
+++ balls_and_walls.py
@@ -2,33 +2,34 @@ from tkinter import *
 from random import randrange as rnd, choice
 import random
 import time
-color=['red','green','yellow','blue','black','pink']
+color = ['red', 'green', 'yellow', 'blue', 'black', 'pink']
 
 root = Tk()
 root.geometry("300x300")
-canvas = Canvas(root,bg='white')
+canvas = Canvas(root, bg='white')
 
 canvas.pack(fill=BOTH, expand=1)
 
-balls=[]
-for i in range(5):    
-    xb=random.randint(50,250)
-    yb=random.randint(50,250)
-    rb=random.randint(20,50)
-    dxb=random.randint(3,10)
-    dyb=random.randint(3,10)
-    ovall=canvas.create_oval(xb-rb, yb-rb, xb+rb, yb+rb, fill = choice(color))
-    ball=[xb,yb,rb,dxb,dyb,ovall]
+balls = []
+for i in range(5):
+    xb = random.randint(50, 250)
+    yb = random.randint(50, 250)
+    rb = random.randint(20, 50)
+    dxb = random.randint(3, 10)
+    dyb = random.randint(3, 10)
+    ovall = canvas.create_oval(xb-rb, yb-rb, xb+rb, yb+rb, fill=choice(color))
+    ball = [xb, yb, rb, dxb, dyb, ovall]
     balls.append(ball)
-    
+
+
 def tick_handler():
     global balls
     for i in range(len(balls)):
-        x, y, r, dx, dy, ovall =balls[i]
+        x, y, r, dx, dy, ovall = balls[i]
         print("Тик!")
-        # Отражение от края холста
         if x < 0:
-            dx = -dx; x = 0
+            dx = -dx
+            x = 0
         elif x > 300-40:
             dx = -dx
             x = 300-40
@@ -41,7 +42,8 @@ def tick_handler():
         x = x + dx
         y = y + dy
         canvas.move(ovall, dx, dy)
-        balls[i]=[x, y, r, dx, dy, ovall]
+        balls[i] = [x, y, r, dx, dy, ovall]
+
 
 def time_handler():
     global freeze
@@ -54,27 +56,24 @@ def time_handler():
     sleep_dt = 1100 - 100*speed
     root.after(sleep_dt, time_handler)
 
+
 def unfreezer(event):
     global freeze
-    if freeze == True:
+    if freeze is True:
         speed = speed_scale.get()
         if speed != 0:
             freeze = False
             root.after(0, time_handler)
 
 
-
-
-           
-
 speed_scale = Scale(root, orient=HORIZONTAL, length=300,
-               from_=0, to=10, tickinterval=1, resolution=1)
+from_=0, to=10, tickinterval=1, resolution=1)
 speed_scale.pack()
 
-# Скорость = 1
-speed_scale.set(1);
+speed_scale.set(1)
 freeze = False
 
 root.after(10, time_handler)
 speed_scale.bind("<Motion>", unfreezer)
 root.mainloop()
+
