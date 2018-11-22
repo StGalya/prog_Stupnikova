81866b55e2b4d5993f17b5f213d1c012bd58f1df
StGalya <stupnikovagalunya@yandex.ru>
2018-11-23 00:34:44 +0300

    исправила по PEP8, 1 балл за 1 клик по шарику

--- ball1.py
+++ ball1.py
@@ -7,24 +7,29 @@ color = ['red', 'green', 'yellow', 'blue', 'black', 'pink']
 
 
 def tick():
-    global x, y, r
+    global xx, yy, r, counter
     root.after(1000, tick)
     canv.delete(ALL)
-    x = random.randint(100, 700)
-    y = random.randint(100, 500)
+    xx = random.randint(100, 700)
+    yy = random.randint(100, 500)
     r = random.randint(20, 100)
-    canv.create_oval(x-r, y-r, x+r, y+r, fill=choice(color))
-    canv.create_text(300, 50, text=counter)
-    canv.create_text(150, 50, text="Попадания:", font='Arial 20')
+    canv.create_oval(xx-r, yy-r, xx+r, yy+r, fill=choice(color))
+    canv.create_text(150, 50, text="Попадания: " + str(counter),
+                     font='Arial 20')
 
 
 def click(event):
+    global xx, yy
     xc = event.x
     yc = event.y
     global counter
-    if (x-xc)**2+(y-yc)**2 < r**2:
+    if (xx-xc)**2+(yy-yc)**2 < r**2:
         counter += 1
-
+        canv.delete(ALL)
+        xx = 1000
+        yy = 1000
+        canv.create_text(150, 50,
+        text="Попадания: " + str(counter), font='Arial 20')         
 root = Tk()
 root.geometry('800x600')
 
@@ -34,3 +39,4 @@ canv.pack(fill=BOTH, expand=1)
 root.after_idle(tick)
 root.bind('<Button-1>', click)
 root.mainloop()
+
