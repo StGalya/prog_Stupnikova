38b8893b0d0ddec88603053dc4e30495d4b0013f
StGalya <stupnikovagalunya@yandex.ru>
2018-12-07 20:03:48 +0300

    сделала функцией, исправила PEP8

--- balls_and_walls.py
+++ balls_and_walls.py
@@ -1,42 +1,51 @@
 from tkinter import *
-from random import randrange as rnd, choice
+import graphics as gr
 import random
-color = ['red', 'green', 'yellow', 'blue', 'black', 'pink']
-
 root = Tk()
-root.geometry("300x300")
+root.geometry("1000x600")
 canvas = Canvas(root, bg='white')
 
 canvas.pack(fill=BOTH, expand=1)
 
 balls = []
 for i in range(5):
+    color = gr.color_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     xb = random.randint(50, 250)
     yb = random.randint(50, 250)
     rb = random.randint(20, 50)
     dxb = random.randint(3, 10)
     dyb = random.randint(3, 10)
-    ovall = canvas.create_oval(xb-rb, yb-rb, xb+rb, yb+rb, fill=choice(color))
+    ovall = canvas.create_oval(xb-rb, yb-rb, xb+rb, yb+rb, fill=color)
     ball = [xb, yb, rb, dxb, dyb, ovall]
     balls.append(ball)
+
+
+def is_collide(x, range, side):
+    if side == 1:
+        if x > range - 100:
+            return (True)
+    if side == 0:
+        if x < 0:
+            return (True)
+    return(False)
 
 
 def tick_handler():
     global balls
     for i in range(len(balls)):
         x, y, r, dx, dy, ovall = balls[i]
-        if x < 0:
+        if is_collide(x, 1000, 1) is True:
+            x = 900
             dx = -dx
+        if is_collide(x, 0, 0) is True:
             x = 0
-        elif x > 300-40:
             dx = -dx
-            x = 300-40
-        if y < 0:
+        if is_collide(y, 600, 1) is True:
+            y = 500
             dy = -dy
+        if is_collide(y, 0, 0) is True:
             y = 0
-        elif y > 300-40:
             dy = -dy
-            y = 300-40
         x = x + dx
         y = y + dy
         canvas.move(ovall, dx, dy)
@@ -51,11 +60,11 @@ def time_handler():
         freeze = True
         return
     tick_handler()
-    sleep_dt = 1100 - 100*speed
+    sleep_dt = 110 - 10*speed
     root.after(sleep_dt, time_handler)
 
 
-def unfreezer(event):
+def unfreezer():
     global freeze
     if freeze is True:
         speed = speed_scale.get()
@@ -65,7 +74,7 @@ def unfreezer(event):
 
 
 speed_scale = Scale(root, orient=HORIZONTAL, length=300,
-from_=0, to=10, tickinterval=1, resolution=1)
+                    from_=0, to=10, tickinterval=1, resolution=1)
 speed_scale.pack()
 
 speed_scale.set(1)
